c81f723bd60ab9bfd2eacbcee7ee59aebcbae12c
XDean <373216024@qq.com>
2018-04-25 15:30:47 +0800

    add javadoc

--- src/main/java/xdean/annotation/processor/toolkit/test/Compile.java
+++ src/main/java/xdean/annotation/processor/toolkit/test/Compile.java
@@ -8,15 +8,35 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 
+/**
+ * Indicate the method is a compile period test. The test class must extends
+ * {@link CompileTest}.
+ *
+ * The annotated method must be public void and one argument
+ * {@link RoundEnvironment} .
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
 @Retention(RUNTIME)
 @Target(METHOD)
 @Documented
 public @interface Compile {
+  /**
+   * The source files to compile. Has same rule of
+   * {@link Class#getResource(String)}.
+   */
   String[] sources();
 
+  /**
+   * Supported annotations. Empty means '*'.
+   */
   Class<? extends Annotation>[] annotations() default {};
 
+  /**
+   * Supported source version.
+   */
   SourceVersion version() default SourceVersion.RELEASE_8;
 }
--- src/main/java/xdean/annotation/processor/toolkit/test/Compiled.java
+++ src/main/java/xdean/annotation/processor/toolkit/test/Compiled.java
@@ -9,13 +9,34 @@ import java.lang.annotation.Target;
 
 import javax.annotation.processing.Processor;
 
+import com.google.testing.compile.Compilation;
+
+/**
+ * Indicate the method is a compiled test. The test class must extends
+ * {@link CompileTest}.
+ *
+ * The annotated method must be public void and one argument
+ * {@link Compilation}.
+ *
+ * @author Dean Xu (XDean@github.com)
+ */
 @Retention(RUNTIME)
 @Target(METHOD)
 @Documented
 public @interface Compiled {
+  /**
+   * The source files to compile. Has same rule of
+   * {@link Class#getResource(String)}.
+   */
   String[] sources();
 
+  /**
+   * Processors to use in this compile.
+   */
   Class<? extends Processor>[] processors() default {};
 
+  /**
+   * Options to use in this compile. Should starts with "-A".
+   */
   String[] options() default {};
 }
