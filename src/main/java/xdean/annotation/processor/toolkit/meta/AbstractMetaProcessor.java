dc6e9a895ad69b95ea92e88218b13fb3510f2209
XDean <373216024@qq.com>
2018-05-14 15:50:29 +0800

    update AbsMetaProcessor
    1. read meta info
    2. write meta info
    3. process meta

--- src/main/java/xdean/annotation/processor/toolkit/meta/AbstractMetaProcessor.java
+++ src/main/java/xdean/annotation/processor/toolkit/meta/AbstractMetaProcessor.java
@@ -1,16 +1,24 @@
 package xdean.annotation.processor.toolkit.meta;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.annotation.Annotation;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import javax.annotation.CheckForNull;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 
 import xdean.annotation.processor.toolkit.AssertException;
+import xdean.annotation.processor.toolkit.ElementUtil;
 import xdean.annotation.processor.toolkit.NestCompileFile;
 import xdean.annotation.processor.toolkit.XAbstractProcessor;
 import xdean.annotation.processor.toolkit.annotation.SupportedMetaAnnotation;
@@ -20,7 +28,7 @@ public abstract class AbstractMetaProcessor<T extends Annotation> extends XAbstr
 
   protected final Class<T> metaClass;
   protected final NestCompileFile metaFile;
-  protected final List<String> annotatedClasses;
+  protected final Set<String> annotatedAnnotationNames;
 
   @SuppressWarnings("unchecked")
   public AbstractMetaProcessor() {
@@ -33,7 +41,7 @@ public abstract class AbstractMetaProcessor<T extends Annotation> extends XAbstr
     metaClass = (Class<T>) meta.value();
     metaFile = new NestCompileFile(META_PATH + meta.value().getCanonicalName());
     try {
-      annotatedClasses = metaFile.readLines().collect(Collectors.toList());
+      annotatedAnnotationNames = metaFile.readLines().collect(Collectors.toSet());
     } catch (IOException e) {
       throw new Error("Fail to read meta file.", e);
     }
@@ -42,17 +50,38 @@ public abstract class AbstractMetaProcessor<T extends Annotation> extends XAbstr
   @Override
   public boolean processActual(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) throws AssertException {
     if (roundEnv.processingOver()) {
+      try (PrintStream printer = metaFile.getPrintStream(filer)) {
+        roundEnv.getElementsAnnotatedWith(metaClass)
+            .stream()
+            .filter(e -> e.getKind() == ElementKind.ANNOTATION_TYPE)
+            .forEach(e -> printer.println(e.asType().toString()));
+      } catch (IOException e) {
+        throw new Error("Error to write meta file.", e);
+      }
       return false;
     }
-    roundEnv.getElementsAnnotatedWith(metaClass);
+    Map<Boolean, List<Element>> map = roundEnv.getElementsAnnotatedWith(metaClass)
+        .stream()
+        .collect(Collectors.groupingBy(e -> e.getKind() == ElementKind.ANNOTATION_TYPE));
+    List<TypeElement> annotatedAnnotations = map.get(true).stream().map(e -> (TypeElement) e).collect(Collectors.toList());
+    List<Element> annotatedElements = map.get(false);
+    annotatedElements.forEach(e -> handleAssert(() -> process(roundEnv, e.getAnnotation(metaClass), null, e)));
+    Stream.concat(
+        annotatedAnnotations.stream(),
+        annotatedAnnotationNames.stream().map(s -> elements.getTypeElement(s)))
+        .forEach(te -> {
+          T meta = te.getAnnotation(metaClass);
+          roundEnv.getElementsAnnotatedWith(te).forEach(
+              e -> handleAssert(() -> process(roundEnv, meta, ElementUtil.getAnnotationMirror(e, te.asType()).get(), e)));
+        });
     return false;
   }
 
-  protected abstract void process(RoundEnvironment env);
+  protected abstract void process(RoundEnvironment env, T t, @CheckForNull AnnotationMirror mid, Element element);
 
   @Override
   public Set<String> getSupportedAnnotationTypes() {
-    Set<String> set = new HashSet<>(annotatedClasses);
+    Set<String> set = new HashSet<>(annotatedAnnotationNames);
     set.add(metaClass.getCanonicalName());
     return set;
   }
